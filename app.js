940a322c15c0760a74c05c526003d0c07296f85a
giosoreira <52332272+giosoreira@users.noreply.github.com>
2019-07-04 20:06:22 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,32 +2,26 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
 import errorhandler from 'errorhandler';
-import dotenv from 'dotenv';
 
-dotenv.config();
-const app = express();
+import * as ProductsModel from './product/model';
+
+//ProductsModel.selectAll(1, console.log);
+// selectUserById(1);
 
 
-// connection.connect(function (err) {
-//     if (err) {
-//         console.error('error connecting: ' + err.stack);
-//         return;
-//     }
+// const app = express();
 
-//     console.log('connected as id ' + connection.threadId);
-// });
-
-app.set('port', process.env.PORT || 3000);
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.set('port', process.env.PORT || 3000);
+// app.use(logger('dev'));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(app.router);
 
-if (process.env.NODE_ENV === 'development') {
-    // only use in development
-    app.use(errorhandler())
-}
+// if (process.env.NODE_ENV === 'development') {
+//     // only use in development
+//     app.use(errorhandler())
+// }
 
-app.listen(app.get('port'), function () {
-    console.log('Express server listening on port ' + app.get('port'));
-});
+// app.listen(app.get('port'), function () {
+//     console.log('Express server listening on port ' + app.get('port'));
+// });
