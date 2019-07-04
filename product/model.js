dc435a52cc3504a3defa7314fe80374c2f42224b
giosoreira <52332272+giosoreira@users.noreply.github.com>
2019-07-04 20:07:02 -0300

    Update model.js

--- product/model.js
+++ product/model.js
@@ -1,26 +1,73 @@
-import db from '../db';
+import { getPool } from '../db';
 
-const pool = db.getPool();
+function add(product) {
+    const pool = getPool();
 
-function add(params) {
     pool.query('INSERT ...', function (error, results, fields) {
         if (error) {
             throw error;
         }
+        console.log(results.insertId);
         // ...
     });
 }
 
-function update(params) {
+function update(product) {
 
 }
 
-function list(params) {
+function selectOneByName(name, userId, callback) {
+    const pool = getPool();
+
+    pool.query('SELECT ... FROM products WHERE ...', function (error, results, fields) {
+        if (error) {
+            throw error;
+        }
+        callback(results)
+        // ...
+    });
+}
+
+function selectAll(userId, callback) {
+    const pool = getPool();
+
+    function queryCallback(error, results, fields) {
+        if (error) {
+            throw error;
+        }
+        // results:
+        // [
+        //     {
+        //       product_id: 1,
+        //       name: 'Café',
+        //       quantity: 3,
+        //       user_id: 1
+        //     },
+        //     {
+        //       product_id: 2,
+        //       name: 'Açucar',
+        //       quantity: 1,
+        //       user_id: 1
+        //     },
+        //    ...
+        //   ]
+
+        // transformar product_id em productId
+        // remover user_id, pois quem chamou essa função já sabe o id do usuário
+        // chamar o callback com a lista tratada
+        callback(results);
+    }
+
+    const sql = 'SELECT * FROM products WHERE user_id = ' + pool.escape(userId);
+
+    pool.query(sql, queryCallback);
+}
+
+function remove(id, userId) {
+    const pool = getPool();
+
+
 
 }
 
-function remove(id) {
-
-}
-
-export { add, update, list, remove };
+export { add, update, selectAll, selectOneByName, remove };
