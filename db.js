835ff61a209d150dfe555187ff476d35bd6799ba
giosoreira <52332272+giosoreira@users.noreply.github.com>
2019-07-04 20:07:57 -0300

    Update db.js

--- db.js
+++ db.js
@@ -3,14 +3,22 @@ import mysql from 'mysql';
 let pool;
 
 function getPool() {
+    console.log('havePool?:', Boolean(pool));
+
     if (pool) return pool;
-    pool = mysql.createPool({
+
+    const connObject = {
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
         port: process.env.DB_PORT
-    });
+    };
+
+    console.log('connObject :', connObject);
+
+    pool = mysql.createPool(connObject);
+
     return pool;
 }
 
