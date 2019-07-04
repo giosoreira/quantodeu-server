560146429397a1ac2a057d501f88d75a544be3ef
giosoreira <52332272+giosoreira@users.noreply.github.com>
2019-07-04 20:07:29 -0300

    Update controller.js

--- product/controller.js
+++ product/controller.js
@@ -1,10 +1,44 @@
-import ProductModel from './model';
+import * as ProductModel from './model';
+import { selectUserById } from '../user/model';
 
-function add(product) {
+/**
+ * 
+ * @param {Object} product
+ * @param {string} product.name
+ * @param {number=} product.quantity
+ * @param {number} userId 
+ * @returns {number} ID do produto recém adicionado / existente
+ */
+function add(product, userId, callback) {
+    // // encontrar o usuário
+    // const user = selectUserById(1);
 
+    // nome do produto não pode ser string vazia, null ou undefined
+    // throw {
+        
+    // }
+
+    // o valor padrão de product.quantity deverá ser 0, caso seja passado 'undefined' ou 'null'
+
+    ProductModel.selectOneByName('nome', 3, function(prod) {
+        
+    })
+
+    // verificar se existe produto com mesmo nome na lista do usuário referente ao userId (case insensitive)
+    // se exitir, não adiciona novo produto, mas atualiza a quantidade
+    ProductModel.update({
+        id: 2,
+        quantity: 3
+    })
+
+    // se não existir, adiciona o novo produto com a quantidade
+    ProductModel.add({
+        name: 'Café',
+        quantity: 3
+    });
 }
 
-function update(params) {
+function update(product) {
 
 }
 
@@ -16,7 +50,4 @@ function remove(id) {
 
 }
 
-export {
-    add,
-    remove
-};
+export { add, update, list, remove };
